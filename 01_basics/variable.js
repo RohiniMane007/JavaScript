51de4b3ddd3c42fe20a86d217b61850a9a4ab165
Rohini Mane <123631971+RohiniMane007@users.noreply.github.com>
2025-01-24 06:47:38 +0000

    new

---
+++ 01_basics/variable.js
@@ -0,0 +1,13 @@
+const accountId = 123
+let accountEmail = "asd@gmail.com"
+var accountPassword = "1234"
+accountCity = "Pune"
+
+
+// accountId = 2 //not allowd
+accountEmail = "sss@g.com"
+accountPassword = "5555"
+accountCity = "Kop"
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity ])
