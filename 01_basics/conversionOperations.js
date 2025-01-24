9db3114f50b93dad169c5e99975de9ccad8ece90
Rohini Mane <123631971+RohiniMane007@users.noreply.github.com>
2025-01-24 11:43:18 +0000

    new

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,40 @@
+
+//******Conversions ************/
+let score = "40"
+
+console.log(typeof score);
+
+let valInt = Number(score) 
+
+console.log(valInt);
+console.log(typeof valInt);
+
+let isBool = Boolean(valInt)
+console.log(typeof isBool);
+console.log(isBool);
+
+
+let strNum = String(valInt); 
+console.log(typeof strNum)
+
+//************Operations *********/
+
+let val = 9
+
+let negVal = -val
+console.log(negVal);
+
+// console.log(2+2);
+// console.log(4-2);
+// console.log(4*2);
+// console.log(4/2);
+// console.log(5%2);
+
+let str1 = "He"
+let str2 = "Hi"
+
+let str3 = str1+str2
+
+console.log(str3);
+console.log(true + true);
+console.log(false + false);
