a6831392fec395e887f094d0aea9f9ff080036a2
Rohini Mane <123631971+RohiniMane007@users.noreply.github.com>
2025-01-25 07:31:26 +0000

    string

---
+++ 01_basics/string.js
@@ -0,0 +1,32 @@
+const firstname = "rohini"
+const lastname = "mane"
+
+// console.log(firstname+lastname+"software developer"); not used
+
+
+console.log(`Hello my name is ${firstname} ${lastname}, I am devloper `)
+
+
+const petName = new String("Caty")
+const petName2 = new String("Caty-Mini-tiny")
+
+console.log(petName);
+console.log(petName[2]);
+console.log(petName.length);
+console.log(petName.toUpperCase());
+console.log(petName.charAt(2));
+console.log(petName.indexOf('a'));
+
+
+console.log(petName2.substring(0,4));
+const newstr = petName2.slice(-8,10)
+console.log(newstr);
+
+const str = "  Hello  World"
+
+console.log(str);
+console.log(str.trim());
+
+console.log(str.replace('World','ding'));
+
+
