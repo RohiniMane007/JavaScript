279aaae37a4f561f2da7b7fe55bfaf0a9d92fbfc
Rohini Mane <123631971+RohiniMane007@users.noreply.github.com>
2025-01-25 10:58:25 +0000

    number

--- 01_basics/number_maths.js
+++ 01_basics/number_maths.js
@@ -1,17 +1,26 @@
 const total = 300
-console.log(total);
+// console.log(total);
 
 
 const sub1 = new Number(200)
-console.log(sub1);
+// console.log(sub1);
 
 const num1 = 123.4568
 
-console.log(num1.toFixed(2))
-console.log(num1.toPrecision(4))
+// console.log(num1.toFixed(2))
+// console.log(num1.toPrecision(4))
 
 const num2 = 1000000
-console.log(num2.toLocaleString());
-console.log(num2.toLocaleString('en-IN'));
+// console.log(num2.toLocaleString());
+// console.log(num2.toLocaleString('en-IN'));
 
 //************Maths ***************/
+
+console.log(Math);
+console.log(Math.abs(-6));
+
+console.log(Math.round(3.2));
+console.log(Math.floor(3.6));
+console.log(Math.ceil(3.6));
+
+console.log(Math.pow(2,3));
