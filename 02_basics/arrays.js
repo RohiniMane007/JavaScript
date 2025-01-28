d1fb393729ddf9ba3ba912afe91f70c54ef9533c
Rohini Mane <123631971+RohiniMane007@users.noreply.github.com>
2025-01-28 09:08:25 +0000

    array

--- 02_basics/arrays.js
+++ 02_basics/arrays.js
@@ -4,8 +4,27 @@ const myArr2 = ["a","b","c","d","e"]
 // console.table([myArr.length, myArr2.length]);
 
 const arr1 = new Array('a','b','c','d','e','f');
+const arr2 = new Array(4,6);
 
 // console.log(arr1.concat(1,2,3));
-// console.log(arr1.at(1));
+// console.log(arr2.length);
 
-console.log(arr1.every);
+// console.log(myArr.shift());
+// console.log(myArr);
+// console.log(myArr.unshift(0));
+// console.log(myArr);
+
+
+// console.log(myArr.push(8));
+// console.log(myArr);
+// console.log(myArr.pop(9));
+// console.log(myArr);
+
+
+// console.log(myArr.slice(1,4));
+// console.log(myArr);
+// console.log(myArr.splice(1,4));
+// console.log(myArr);
+
+console.log(myArr.indexOf(2));
+console.log(myArr.reduce((prev,curr) => prev<curr));
