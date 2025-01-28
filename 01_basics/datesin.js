4c4491a4bbe3153a21a319d11acf91c7d4be8857
Rohini Mane <123631971+RohiniMane007@users.noreply.github.com>
2025-01-28 05:22:26 +0000

    date

--- 01_basics/datesin.js
+++ 01_basics/datesin.js
@@ -9,9 +9,22 @@ console.log(myDate.toTimeString()); //04:45:12 GMT+0000 (Coordinated Universal T
 
 console.log(myDate.toLocaleDateString()); //1/28/2025
 console.log(myDate.toLocaleString()); //1/28/2025, 4:45:12 AM
+console.log(myDate.toLocaleString('default',{
+    weekday: "short"
+})); //1/28/2025, 4:45:12 AM
 
 console.log(myDate.toLocaleTimeString()); //4:45:12 AM
 console.log(myDate.toTimeString()); //04:45:12 GMT+0000 (Coordinated Universal Time)
 
 console.log(myDate.toUTCString()); //Tue, 28 Jan 2025 04:45:12 GMT
 console.log(myDate.toJSON()); //2025-01-28T04:47:58.111Z
+
+let myNewDate = new Date("01-14-2025"); //mm-dd-yyyy
+console.log(myNewDate.toLocaleString());
+
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+console.log(myNewDate.getTime());
+console.log(Math.floor(Date.now()/1000));
