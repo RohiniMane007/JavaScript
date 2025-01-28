1a9f878dc4e080eef4b8af8c2201b244a50a7637
Rohini Mane <123631971+RohiniMane007@users.noreply.github.com>
2025-01-28 06:16:07 +0000

    array

---
+++ 02_basics/arrays.js
@@ -0,0 +1,11 @@
+
+const myArr = [0,1,2,3,4,5,6,7]
+const myArr2 = ["a","b","c","d","e"]
+// console.table([myArr.length, myArr2.length]);
+
+const arr1 = new Array('a','b','c','d','e','f');
+
+// console.log(arr1.concat(1,2,3));
+// console.log(arr1.at(1));
+
+console.log(arr1.every);
