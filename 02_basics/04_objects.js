ebb3a2a07decb05d444ba2e30d181b65c05b7a47
Rohini Mane <123631971+RohiniMane007@users.noreply.github.com>
2025-01-29 11:56:11 +0000

    object

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -3,3 +3,27 @@ const temp = new Object()
 temp.id = 1
 temp.name = "dddd"
 temp.contact = "asd"
+
+
+// console.log(temp);
+
+const user = {
+    id: "asd",
+    fullname: {
+        firstname: "ggggg",
+        lastname: "hhhhh"
+    }
+}
+
+// console.log(user);
+// console.log(user.fullname);
+
+const obj1 = { 1: "a", 2: "b" }
+const obj2 = { 3: "c", 4: "d" }
+
+// const obj3 = {obj1 , obj2};
+// console.log(obj3);
+
+const obj3 = Object.assign({},obj1 , obj2);
+console.log(obj3);
+
