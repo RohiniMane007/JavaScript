ed4b27f15fb05c4a7adccd72f3c21d693021aae5
Rohini Mane <123631971+RohiniMane007@users.noreply.github.com>
2025-01-31 07:05:45 +0000

    scope

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -1,13 +1,56 @@
-
-var c = 300
+var a = 11
+let c = 300
 if (true) {
-    let a = 10
+    var a = 10
     const b = 20
-    var c = 30
+    let c = 30
 
-    // console.log(a);
+    // console.log("inner:" , c);
 }
 
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
+
+function one(){
+    const user = "rohini"
+
+    function two(){
+        const id = "1111"
+        console.log(user);
+        console.log(id);
+    }
+
+    // console.log(id)
+    two()
+}
+// one()
+
+if(true){
+    const user = "aaaa"
+    if(user ==="aaaa"){
+        const id = "111"
+
+        // console.log(user + id);
+    }
+
+    // console.log(id);
+}
+// console.log(user);
+
+//==================================================//
+
+function add(num){
+    return num+1
+}
+
+add(5)
+console.log(add(5));
+
+const addtwo = function(num){
+    return num +2
+}
+
+// addtwo(7)
+
+// console.log(addtwo(7))
