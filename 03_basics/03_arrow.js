0c4d4c2c99eee7e1cb9c7a39335c1c760d0df18f
Rohini Mane <123631971+RohiniMane007@users.noreply.github.com>
2025-01-31 09:17:01 +0000

    arrow fun

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -3,10 +3,36 @@ const user ={
     price: 100,
 
     welcomeMsg : function(){
-        console.log(`welcom ${user.user}`)
+        // console.log(`welcom ${this.user}`)
     }
 }
 
 user.welcomeMsg();
 user.user = "new"
 user.welcomeMsg()
+
+// console.log(this);
+// function fun(){
+//     let username = 'ggggggg'
+//     console.log(username);
+// }
+
+// fun()
+
+
+// const fun =  ()=>{
+//     let username = 'ggggggg'
+//     console.log(this);
+// }
+
+// fun()
+
+// const sum = (num1,num2) => {
+//     return num1+num2
+// }
+
+// const sum = (num1,num2) =>  num1+num2
+const sum = (num1,num2) =>  (num1+num2)
+
+
+console.log(sum(2,7));
