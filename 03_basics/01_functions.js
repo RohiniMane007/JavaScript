40e3e591e7b64b9d60db4a19ecf653dc5eb6ef0b
Rohini Mane <123631971+RohiniMane007@users.noreply.github.com>
2025-01-31 05:23:22 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,23 @@
+function  viewFun(){
+    console.log("function");
+}
+
+// viewFun();
+
+// function addNum(num1, num2){
+//     console.log(num1+num2);
+// }
+
+// addNum() //NaN
+// addNum(4,3) // 7
+// addNum(4,"7") // 47
+// addNum(4,"a") // 4a
+// addNum(4,null) 
+
+function addNum(num1, num2){
+    let result = num1+num2
+
+    return result;
+}
+
+const result = addNum(5,3)
