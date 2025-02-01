d23a8601b1660694768cea014f70224cd01544d7
Rohini Mane <123631971+RohiniMane007@users.noreply.github.com>
2025-02-01 06:32:54 +0000

    if-else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,23 @@
+let isLoggedIn = true;
+const temperature = 41
+// if(isLoggedIn){
+//     console.log("executed");
+// }
+
+// if(5>2){
+//     console.log("executed");
+// }
+// <, >, <=, >=, ==, !=, ===, !==
+
+if(temperature === 41){
+    console.log("temperature less than 50");
+}else{
+    console.log("temperature greter than 50")
+}
+
+const score = 200
+
+if(score>100){
+    const power = 'fly'
+    console.log(`user power: ${power}`);
+}
