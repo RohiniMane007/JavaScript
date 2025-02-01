9f46196235b5d75d994eca61354b71dea0609a3b
Rohini Mane <123631971+RohiniMane007@users.noreply.github.com>
2025-02-01 07:17:03 +0000

    new

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,24 @@
+let month = 4
+
+switch (month) {
+    case 1:
+        console.log("Jan");
+        break;
+
+    case 2:
+        console.log("Feb");
+        break;
+
+    case 3:
+        console.log("Mar");
+        break;
+
+    case 3:
+        console.log("Apr");
+        break;
+
+    default:
+        console.log("default");
+
+        break;
+}
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,44 @@
+const data = ""
+if (data) {
+    console.log("true");
+    
+} else {
+    console.log("false");
+    
+}
+
+const data2 = []
+if (data2) {
+    // console.log("true");
+    
+} else {
+    // console.log("false");
+    
+}
+
+//falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy value
+
+// "0",'false', " ", [], {}, function(){}
+
+
+const obj = {}
+
+if(Object.keys(obj).length == 0){
+    console.log("empty");
+    
+}
+
+//nullish coalescing operator (??): null undefined
+
+let val;
+// val = 5 ?? 10;
+// val = null ?? 10;
+// val = undefined ?? 15;
+
+
+val = null ?? 10 ?? 20;
+console.log(val);
