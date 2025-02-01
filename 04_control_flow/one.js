b5a63142dd801f096f6e6ac3f11a8abe0a39fc9e
Rohini Mane <123631971+RohiniMane007@users.noreply.github.com>
2025-02-01 06:41:40 +0000

    if-else

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -10,14 +10,30 @@ const temperature = 41
 // <, >, <=, >=, ==, !=, ===, !==
 
 if(temperature === 41){
-    console.log("temperature less than 50");
+    // console.log("temperature less than 50");
 }else{
-    console.log("temperature greter than 50")
+    // console.log("temperature greter than 50")
 }
 
 const score = 200
 
 if(score>100){
     const power = 'fly'
-    console.log(`user power: ${power}`);
+    // console.log(`user power: ${power}`);
+}
+
+const balance = 600
+
+if (balance < 500) {
+    console.log("less than 500");
+    
+} else if(balance < 750){
+    console.log("less than 750");
+
+} else if (balance < 900){
+    console.log("less than 900");
+
+}else{
+    console.log("less than 1200");
+
 }
