e2ee4391219f2379300836d2940ee4ed87e80418
Rohini Mane <123631971+RohiniMane007@users.noreply.github.com>
2025-02-05 09:45:48 +0000

    project

---
+++ 07_projects/script.js
@@ -0,0 +1,19 @@
+const red = document.querySelector(".red")
+const cyan = document.querySelector(".cyan")
+const violet = document.querySelector(".violet")
+const orange = document.querySelector(".red")
+const pink = document.querySelector(".pink")
+
+const center = document.querySelector(".center")
+
+// console.log(window.getComputedStyle().backgroundColor);
+
+
+// const getBGColor = (selectedElement) => {
+//     return window.getComputedStyle(selectedElement).backgroundColor;
+// }
+// var orangeElementColor = getBGColor(orange);
+
+// orange.addEventListener('mouseenter',()=>{
+//     center.style.background = orangeElementColor
+// })
