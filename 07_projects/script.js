4d9cfb481f58f2a33770f282c6d2f9b5e5a88473
Rohini Mane <123631971+RohiniMane007@users.noreply.github.com>
2025-02-05 10:52:32 +0000

    color switch

--- 07_projects/script.js
+++ 07_projects/script.js
@@ -4,23 +4,47 @@ const buttons = document.querySelectorAll('.button');
 const body = document.querySelector('body');
 console.log(buttons);
 
-buttons.forEach((button)=>{
+buttons.forEach((button) => {
     // console.log(button)
-    button.addEventListener('click',function(e){
+    button.addEventListener('click', function (e) {
         // console.log(e);
         // console.log(e.target);
-       if (e.target.id === 'grey') {
-        console.log(e.target.id);
-        body.style.backgroundColor = e.target.id
-       } else if (e.target.id === 'white') {
-        console.log(e.target.id);
-        body.style.backgroundColor = e.target.id
-       } else if (e.target.id === 'purple') {
-        console.log(e.target.id);
-        body.style.backgroundColor = e.target.id
-       } else if (e.target.id === 'plum') {
-        console.log(e.target.id);
-        body.style.backgroundColor = e.target.id
-       } 
+        /* if (e.target.id === 'grey') {
+          console.log(e.target.id);
+          body.style.backgroundColor = e.target.id
+         } else if (e.target.id === 'white') {
+          console.log(e.target.id);
+          body.style.backgroundColor = e.target.id
+         } else if (e.target.id === 'purple') {
+          console.log(e.target.id);
+          body.style.backgroundColor = e.target.id
+         } else if (e.target.id === 'plum') {
+          console.log(e.target.id);
+          body.style.backgroundColor = e.target.id
+         } */
+
+        switch (e.target.id) {
+            case 'violet':
+                body.style.backgroundColor = e.target.id;
+                break;
+            case 'indigo':
+                body.style.backgroundColor = e.target.id;
+                break;
+            case 'blue':
+                body.style.backgroundColor = e.target.id;
+                break;
+            case 'green':
+                body.style.backgroundColor = e.target.id;
+                break;
+            case 'yellow':
+                body.style.backgroundColor = e.target.id;
+                break;
+            case 'orange':
+                body.style.backgroundColor = e.target.id;
+                break;
+            case 'red':
+                body.style.backgroundColor = e.target.id;
+                break;
+        }
     })
 });
