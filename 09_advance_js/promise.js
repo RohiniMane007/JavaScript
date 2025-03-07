ab98ee336989846a2790bc2d1c3f1edde4116afd
Rohini Mane <123631971+RohiniMane007@users.noreply.github.com>
2025-03-07 10:44:05 +0000

    change

--- 09_advance_js/promise.js
+++ 09_advance_js/promise.js
@@ -73,3 +73,12 @@ async function consumePromiseFive(){
 }
 
 consumePromiseFive()
+
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
